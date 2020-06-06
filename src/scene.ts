d2f830364c5d2e5977e9ea2b3f4ee6d4699c1620
@dankomorny <DanKomorny@users.noreply.github.com>
2020-06-06 11:11:35 +0300

    babylon-vrm-loader update

--- src/scene.ts
+++ src/scene.ts
@@ -2,6 +2,7 @@ import * as BABYLON from "@babylonjs/core";
 //import * as LOADERS from "@babylonjs/loaders";
 //import * as MATERIALS from "@babylonjs/materials";
 //import * as GUI from "@babylonjs/gui";
+//import * as INSPECTOR from "@babylonjs/inspector";
 import "babylon-vrm-loader";
 
 export default class Game {
@@ -10,41 +11,37 @@ export default class Game {
   constructor(elem: HTMLCanvasElement) {
     this.engine = new BABYLON.Engine(elem, true);
     this.scene = new BABYLON.Scene(this.engine);
-    //Inspector Code
+    //Inspector
     //this.scene.debugLayer.show();
   }
 
   async setup(): Promise<void> {
-    //Create default camera
-    this.scene.createDefaultCameraOrLight(false, true, true); // Free Camera settngs
-    //Create ground
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 5, height: 5 });
-    //Ground position
-    ground.position.y -= 1;
-    //import VRM
-    // vrmFile is File object retrieved by <input type="file">.
-    const scene = await BABYLON.SceneLoader.LoadAsync(
-      "https://cdn.jsdelivr.net/gh/DanKomorny/model-storage@105459ba92d4b84675cb183f03177f71d8f20145/Vivi.vrm",
-      "",
-      this.engine
-    );
-    const vrmManager = scene.metadata.vrmManagers[0];
+    {
+      this.scene.createDefaultCameraOrLight(false, true, true);
+      const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 5, height: 5 });
+      ground.position.y -= 1;
 
-    // Update secondary animation
-    scene.onBeforeRenderObservable.add(() => {
-      vrmManager.update(scene.getEngine().getDeltaTime());
+      // vrmFile is File object retrieved by <input type="file">.
+      // or you can use url too.
+      BABYLON.SceneLoader.Append(
+        "https://cdn.jsdelivr.net/gh/DanKomorny/model-storage@105459ba92d4b84675cb183f03177f71d8f20145/Vivi.vrm",
+        null,
+        this.scene
+      );
+    }
+    const vrmManager = this.scene.metadata.vrmManagers[0];
+
+    // Update secondary animation(Spring Bones)
+    this.scene.onBeforeRenderObservable.add(() => {
+      vrmManager.update(this.scene.getEngine().getDeltaTime());
     });
-
     // Model Transformation
-    vrmManager.rootMesh.translate(new BABYLON.Vector3(1, 0, 0), 1);
-
+    vrmManager.rootMesh.translate(new BABYLON.Vector3(1, 0, 0), 0);
     // Work with HumanoidBone
     vrmManager.humanoidBone.leftUpperArm.addRotation(0, 1, 0);
-
     // Work with BlendShape(MorphTarget)
     vrmManager.morphing("Joy", 1.0);
   }
-
   run(): void {
     this.engine.runRenderLoop(() => this.scene.render());
   }
